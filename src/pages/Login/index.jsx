f04579b3a15a52f52281fe3afee3b1fc64446994
Gery Wibowo <akun.gery@gmail.com>
2019-10-06 21:29:54 +0700

    layout for login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import { Form, Icon, Input, Button, Checkbox, Row, Col } from 'antd'
+import './style.css'
 
 const LoginPage = () => {
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <>Login</>
-    </div>
+    <Row type="flex" justify="center">
+      <Col span={8} className="login-form">
+        <Form onSubmit={handleSubmit} className="login-form">
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Checkbox>Remember me</Checkbox>
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
   )
 }
 
