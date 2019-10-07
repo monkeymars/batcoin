d45d0356b86b8234f86e5a9742d88a5f45749d8c
Gery Wibowo <akun.gery@gmail.com>
2019-10-08 01:52:50 +0700

    authentication on /exchange route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+import cookie from 'js-cookie'
 import Login from './pages/Login'
 import Exchange from './pages/Exchange'
 import Notfound from './pages/404'
 import Navbar from './components/Navbar'
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      cookie.get('authenticated') ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+)
 
 const routing = (
   <Router>
@@ -12,7 +32,7 @@ const routing = (
       <Navbar />
       <Switch>
         <Route exact path="/" component={Login} />
-        <Route path="/exchange" component={Exchange} />
+        <PrivateRoute path="/exchange" component={Exchange} />
         <Route exact path="/login" component={Login} />
         <Route component={Notfound} />
       </Switch>
